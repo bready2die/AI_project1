a0fb9aed8d970487b65066ce59b673c66a88440f
Henry Chen <hc867@scarletmail.rutgers.edu>
2022-09-16 00:04:23 -0400

    Added more to grid.c, including an attempt at a successor algorithm

--- grid.c
+++ grid.c
@@ -8,6 +8,19 @@ struct grid
 	//path data may or may not be stored here, tbd
 }
 
+struct coords
+{
+	int x, y;
+};
+
+struct vertex // this setup may change, maybe
+{
+	struct coords;
+	float g; //distance from start, following path
+	float h; //heuristic (estimated distance from vertex to goal)
+	struct vertex* parent;
+}
+
 int read_file(struct grid* grid, char* filename)
 {
 	//INIT new struct grid
@@ -38,6 +51,61 @@ int read_file(struct grid* grid, char* filename)
 
 	if (fclose(file))
 	return 0;
+}
+
+static int succ(struct grid* grid, struct coords pos, struct coords* buffer) //returns successor count
+{
+	int count = 0;
+
+	char flags = 0xf; //flags = 0x1111
+	/*	i=0, 0b1000 -> north west	i=1, 0b0100 -> south west
+		i=2, 0b0010 -> north east	i=3, 0b0001 -> south east */
+
+	//checks diagonally adjacent tiles
+	for (int i = 0; i < 4; i++)
+	{
+		int cell_x = pos.x + i/2 - 1;
+		int cell_y = pos.y + i%2 - 1;
+		//checks if tile is empty
+		if (cell_x >= 0 && cell_y >= 0 && cell_x < grid->width && cell_y < grid->height 
+			&& grid->blocks[cell_x*(grid->height) + cell_y] == 0)
+		{
+			//unmark flag and add diagonal neughbor
+			flags &= ~(1 << (3-i));
+			buffer[count] = (struct coords)
+			{
+				.x=pos.x + (i/2)*2 - 1,
+				.y=pos.y + (i%2)*2 - 1
+			};
+			count++;
+		}
+	}
+	//check cardinal neighbors
+	for (int i = 0; i < 2; i++)
+	{				//	i=0		i=1
+		char xmask = 0xc >> (2*i);//	0b1100 (west)	0b0011 (east)
+		char ymask = 0xa >> i;	//	0b1010 (north)	0b0101 (south)
+
+		if (~flags & xmask)// check if either west/east tile is empty
+		{
+			buffer[count] = (struct coords)//add neighbor
+        	        {
+			        .x=pos.x + i*2 - 1,
+				.y=pos.y
+			};
+			count++;
+		}
+		if (~flags & ymask)// check if either north/south tile is empty
+                {
+                        buffer[count] = (struct coords)//add neighbor
+                        {
+                                .x=pos.x,
+                                .y=pos.y + i*2 - 1
+                        };
+                        count++;
+                }
+	}
+	return count;
 }
 
 void close_grid(struct grid* grid)//note:does not free the pointer itself
